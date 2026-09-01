61ddcd68391a1da89c39f6ece209febd0e265b00
cahyofahma-ti-1c <adanicahyo26@gmail.com>
2026-09-01 15:35:39 +0700

    Add files via upload

---
+++ DASPRO/PROJECT/Latihan08.java
@@ -0,0 +1,9 @@
+public class Latihan08 {
+    public static void main(String[] args) {
+        System.out.println("Nama Saya Cahyo Fahma A");
+        System.out.println("Belajar Java Sangat Menyenangkan");
+        System.out.println("Semakin Sering Mencoba, Semakin Mahir");
+        System.out.print("\nBelajar Java tidaklah sulit, yang sulit adalah malas untuk belajar");
+        System.out.print("Teruslah berlatih dan jangan mudah menyerah");
+    }
+}
